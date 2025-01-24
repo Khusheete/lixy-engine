8550bec6bc8d6bdf13811962018321a1ef08b7fa
Souchet Ferdinand <Siandfrance@gmail.com>
2025-01-24 23:45:50 +0100

    Remove clamp parameter on textures

--- renderer/src/primitives/texture.cpp
+++ renderer/src/primitives/texture.cpp
@@ -135,8 +135,8 @@ namespace lixy::opengl {
 
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+        // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+        // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
 
         glTexImage2D(GL_TEXTURE_2D, 0, internal_format, p_width, p_height, 0, gl_format, GL_UNSIGNED_BYTE, p_data);
 

f94f7b7881676fbaf8a638c9c21364732007cc07
Souchet Ferdinand <Siandfrance@gmail.com>
2025-01-25 01:43:07 +0100

    Filter out linux specific imports

--- demo/build.lua
+++ demo/build.lua
@@ -4,7 +4,7 @@ project "Demo"
     cppdialect "c++17"
     
     files { "src/**.hpp", "src/**.cpp" }
-    links { "Renderer", "Flecs", "Stb", "Core", "WavefrontLoader", "rgfw", "GL", "glad", "pthread", "dl" }
+    links { "Renderer", "Flecs", "Stb", "Core", "WavefrontLoader", "rgfw", "glad" }
 
     build_target_dir()
     build_object_dir()
@@ -16,5 +16,5 @@ project "Demo"
     build_config()
 
     filter "platforms:linux"
-        links { "X11", "Xrandr" }
+        links { "GL", "X11", "Xrandr", "pthread", "dl" }
     filter ""
